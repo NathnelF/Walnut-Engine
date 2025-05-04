5d97b3985e7856f417120e94a8b7c8b1585f3a13
Nate <natefuller8@gmail.com>
2025-05-04 13:47:08 -0400

    Created 'CreateProject' class and added bidnings to the user control .xaml

--- Walnut Editor/GameProject/CreateProject.cs
+++ Walnut Editor/GameProject/CreateProject.cs
@@ -9,7 +9,33 @@ namespace WalnutEditor.GameProject
 {
     class CreateProject : ViewModelBase
     {
+        private string _name = "New Project";
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
 
+        private string _path = $"C:/...";
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                if (_path != value)
+                {
+                    _path = value;
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
     }
 
     
